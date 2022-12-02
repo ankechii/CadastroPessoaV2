920a6e7e96bfad6134f62674640caf457e575e92
ALUNOS\matheus.santos24 <avelinosantos09@gmail.com>
2022-12-01 21:34:51 -0300

    Implementação da criptografia na controller dos dados

---
+++ CadastroPessoas/Cryptography/HashContent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public static class HashContent
+{
+    public static string Hash(this string value)
+    {
+        var key = "$2a$06$.rCVZVOThsIa97pEDOxvGu";
+
+        return BCrypt.Net.BCrypt.HashPassword(value, key);
+    }
+}
